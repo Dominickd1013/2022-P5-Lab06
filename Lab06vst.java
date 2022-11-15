7741e9529011cca835c65bec7c533664ae5056ff
Dominick <domenico.damato@vtsd.com>
2022-11-15 13:12:10 -0500

    Starting lab06.

--- Lab06vst.java
+++ Lab06vst.java
@@ -19,8 +19,18 @@ public class Lab06vst extends Applet
 
 
         // Draw Random Lines
+        Random rndint  = new Random(12345);
+        for(int x = 1; x <= 50; x++)
+        {
+            int x1 = rndint.nextInt(50)
+            int y1 = rndint.nextInt(50)
+            int x2 = rndint.nextInt(150)
+            int y2 = rndint.nextInt(150)
 
-
+            g.setColor(new Color(225, 0, 0));
+            g.drawLine(x1, y2, x2, y2);
+            x += 1;
+        }
 
 
 
